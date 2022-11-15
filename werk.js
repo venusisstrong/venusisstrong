59cae895350263800afa89c4a9e82f5f015b309a
venusisstrong <bakerhouse89@gmail.com>
2022-11-16 01:30:31 +0800

    four popup

--- werk.js
+++ werk.js
@@ -1,36 +1,47 @@
-//werk page modal 4 popup
 // Get the modal
 var modal = document.getElementById('myModal');
-
 var modal2 = document.getElementById('myModal2');
+var modal3 = document.getElementById('myModal3');
+var modal4 = document.getElementById('myModal4');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 var span2 = document.getElementsByClassName("close2")[0];
+var span3 = document.getElementsByClassName("close3")[0];
+var span4 = document.getElementsByClassName("close4")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
-
 btn2.onclick = function() {
     modal2.style.display = "block";
+}
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+btn4.onclick = function() {
+    modal4.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
-// When the user clicks on <span> (x), close the modal
 span2.onclick = function() {
     modal2.style.display = "none";
+}
+span3.onclick = function() {
+    modal3.style.display = "none";
+}
+span4.onclick = function() {
+    modal4.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -40,5 +51,11 @@ window.onclick = function(event) {
     }
     if (event.target == modal2) {
         modal2.style.display = "none";
+    }
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+    if (event.target == modal4) {
+        modal4.style.display = "none";
     }
 }
